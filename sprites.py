e4840f623fa38337a7c6adc986d510bf0fba26e0
Paxton Weathers <paxton@drab.org>
2025-09-18 17:19:21 -0500

    begin implementing sprites

---
+++ sprites.py
@@ -0,0 +1,10 @@
+import pygame
+
+def load_sprites():
+    return [
+        {
+            'id': 0,
+            'img': pygame.image.load("img/robo_walk.png"),
+            'type': "animation"
+        }
+    ]
