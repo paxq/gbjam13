1ee92e216d27378a8edcd7ea221e6e53c5596678
Corwin Dennis <Corwon52@gmail.com>
2025-09-17 20:24:11 -0500

    movement better! +jumping still nees work

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ clock = pygame.time.Clock()
 deltaTime = 0
 running = True
 
-playerImg = pygame.image.load('Assets/Player_placeholder.png')
+
 # IMG & SOUND SAVED
 # font = pygame.font.SysFont("Arial", 20)
 # text = font.render("Hello World!", False, (255, 255, 255))
@@ -32,71 +32,73 @@ class WorldEvent:
         self.event = function
 
 class Player:
-    def __init__(self, x, y, scaleX=(16 * SCALE_MODIFIER), scaleY=(16 * SCALE_MODIFIER)):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.width = scaleX
-        self.height = scaleY
 
-        self.speed = 7 * SCALE_MODIFIER
-        self.velocityX = 1
-        self.velocityY = 1
-        self.jump_height = self.height * 1.5
-        self.jump_multiplier = 0.99
-        self.initial_height = 0
+        self.width = 16 * SCALE_MODIFIER
+        self.height = 16 * SCALE_MODIFIER
 
-        self.gravity = 0.2
-        self.is_jumping = False
-        self.is_grounded = True
+        self.speed = 0.2
+        self.velocityX = 0
+        self.velocityY = 0
+        self.jump_strength = 2 # for some reson this acts like an exponetal sacle but it works
+        self.gravity = 0.175
+        self.is_grounded = False
+        self.jumping = 1
+
+        self.playerImg = pygame.image.load('Assets/Player_placeholder.png')
+        self.playerImg = pygame.transform.scale(self.playerImg, (self.width, self.height))
 
     def move(self):
         # dx = 0
         # dy = 0
 
         self.velocityX /= 2
-        self.velocityY /= 2
+        self.velocityY /= 1.4
 
-        if self.y >= SCREEN_HEIGHT * SCALE_MODIFIER - self.height:
+        if self.y == 8: # will need to chage this to accept colidble blocks as valid is_grouded spaces
             self.is_grounded = True
-        if self.initial_height - self.y >= self.jump_height:
-            self.is_jumping = False
+
 
         # Get keypresses
         key = pygame.key.get_pressed()
         if key[pygame.K_w] or key[pygame.K_SPACE] or key[pygame.K_UP]:
             if self.is_grounded:
-                self.initial_height = self.y
                 self.is_grounded = False
-                self.is_jumping = True
-            if self.initial_height - self.y < self.jump_height and self.is_jumping:
-                self.velocityY -= 0.4
-
+                self.jumping = 5
         if key[pygame.K_a] or key[pygame.K_LEFT]:
-            self.velocityX -= 0.1
+            self.velocityX -= self.speed
         if key[pygame.K_d] or key[pygame.K_RIGHT]:
-            self.velocityX += 0.1
+            self.velocityX += self.speed
+
+        if self.jumping > 0:
+            self.velocityY -= self.jump_strength * 0.2
+            self.jumping -= 1
+
 
         self.velocityY += self.gravity
 
+        self.x += self.velocityX
+
+        self.y += self.velocityY
+
         # Check for collisions
         # (add world first)
-        if self.y - self.velocityY < 0:
-            self.velocityY += 0.1
-        if self.y + self.velocityY > SCREEN_HEIGHT * SCALE_MODIFIER - self.height:
-            self.velocityY -= 0.2
-        if self.x - self.velocityX < 0:
-            self.velocityX += 0.1
-        if self.x + self.velocityX > SCREEN_WIDTH * SCALE_MODIFIER - self.width:
-            self.velocityX -= 0.1
+        if self.y < 0:
+            self.y = 0
+        if self.y > 8:
+            self.y = 8
+        if self.x < 0:
+            self.x = 0
+        if self.x > 9:
+            self.x = 9
 
-        self.x += self.velocityX * self.speed
-        self.y += self.velocityY * self.speed
 
-    def draw(self, surface):
-        surface.blit(playerImg, (self.x, self.y, self.width, self.height))
-
-    def update(self, events):
-        pass
+    def draw(self):
+        screen.blit(self.playerImg, ((self.x * 16 * SCALE_MODIFIER), ((self.y * 16 * SCALE_MODIFIER)), self.width, self.height))
+    #def update(self, events):
+    #    pass
 
 class Game:
     def __init__(self):
@@ -126,9 +128,6 @@ class Game:
         # draw y lines
         for y in range(0, block_per_width):
             pygame.draw.line(screen, (255, 255, 255), (((SCREEN_WIDTH * SCALE_MODIFIER) / block_per_width) * y, 0), (((SCREEN_WIDTH * SCALE_MODIFIER) / block_per_width) * y, SCREEN_HEIGHT * SCALE_MODIFIER))
-    
-    def draw_player(self, playerPosX, playerPosY):
-        screen.blit(playerImg, (7 + (playerPosX * 64), (7 + (playerPosY * 64))))
 
         
 game = Game()
@@ -137,11 +136,11 @@ player = Player(5, 5)
 async def main():
     global game, deltaTime, clock
 
-    PosX = 5
-    PosY = 5
-    Xvelocity = 0
-    Yvelocity = 0
-    grounded = False
+    #PosX = 5
+    #PosY = 5
+    #Xvelocity = 0
+    #Yvelocity = 0
+    #grounded = False
 
     while game.running:
         game.handle_pygame_events(pygame.event.get())
@@ -149,7 +148,7 @@ async def main():
         game._debug_draw_grid(screen)
 
         player.move()
-        player.draw(screen)
+        player.draw()
         
         pygame.display.update()
 
