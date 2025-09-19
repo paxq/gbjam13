88ac853d64cff9cda7025d4b9902b248e3f73681
Paxton Weathers <paxton@drab.org>
2025-09-19 00:37:55 -0500

    still needs some fixes

--- main.py
+++ main.py
@@ -76,7 +76,6 @@ class World:
     def move(self, x, y):
         self.x += x
         self.y += y
-
         # for background in self.backgrounds:
         #     background.move(x, y)
         for tile in self.tiles:
